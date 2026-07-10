39fd12282c414512a02ada1327efc184863b0e68
Rajneesh Anand <rajneesh.1491981@gmail.com>
2026-07-10 11:42:19 -0400

    Add build/droplet_state/share/droplet_state/hook/pythonpath_develop.dsv, build/droplet_state/share/droplet_state/hook/pythonpath_develop.ps1, build/droplet_state/share/droplet_state/hook/pythonpath_develop.sh

---
+++ build/droplet_state/share/droplet_state/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/rajneesh/droplet_runner_ws/build/droplet_state"
